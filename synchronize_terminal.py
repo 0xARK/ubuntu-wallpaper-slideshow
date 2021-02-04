59ef09a281281fdff1e737c49516d2d709eaf31b
Matéo <67783173+0xARK@users.noreply.github.com>
2021-02-04 16:07:38 +0100

    Update synchronize_terminal.py

--- synchronize_terminal.py
+++ synchronize_terminal.py
@@ -8,6 +8,9 @@ def get_wallpaper():
     return uri
 
 # initialize first shell colours
-current_wallpaper = get_wallpaper()
-current_wallpaper_path = current_wallpaper[7:len(current_wallpaper)]
-os.system("wal --vte -i " + current_wallpaper_path)
+try:
+    current_wallpaper = get_wallpaper()
+    current_wallpaper_path = current_wallpaper[7:len(current_wallpaper)]
+    os.system("wal --vte -i " + current_wallpaper_path)
+except:
+    print("Shell colours synchronization failed.")
