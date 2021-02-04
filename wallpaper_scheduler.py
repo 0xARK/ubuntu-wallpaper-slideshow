f98df98a9898d6585bdff40329c50db211fead58
Matéo <67783173+0xARK@users.noreply.github.com>
2021-02-04 15:59:45 +0100

    Update wallpaper_scheduler.py

--- wallpaper_scheduler.py
+++ wallpaper_scheduler.py
@@ -4,7 +4,7 @@ import os
 
 # edit these variables to match with your needs
 # use only absolute path
-wallpapers_path = "/absolute/path/to/your/wallpaper/folder"
+wallpapers_path = "/home/mateo/Images/Wallpapers"
 seconds_interval = 0
 minutes_interval = 0
 hours_interval = 1
@@ -20,9 +20,13 @@ def change_wallpaper():
     os.system("file=$(ls " + wallpapers_path + " | shuf -n 1) && wal --vte -i " + wallpapers_path + "/$file")
 
 # initialize first shell colours
-current_wallpaper = get_wallpaper()
-current_wallpaper_path = current_wallpaper[7:len(current_wallpaper)]
-os.system("wal --vte -i " + current_wallpaper_path)
+try:
+	change_wallpaper()
+	current_wallpaper = get_wallpaper()
+	current_wallpaper_path = current_wallpaper[7:len(current_wallpaper)]
+	os.system("wal --vte -i " + current_wallpaper_path)
+except:
+	print("Shell colours synchronization failed.")
 
 # scheduler setup
 scheduler = BlockingScheduler()
